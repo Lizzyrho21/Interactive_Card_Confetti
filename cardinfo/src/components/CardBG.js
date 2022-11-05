7d5aa8279181190faa629c3e639caa51e3cc8f63
Lizzy Wiley <Lizzyrho@gmail.com>
2022-11-05 15:31:57 -0500

    mobile card design done

--- cardinfo/src/components/CardBG.js
+++ cardinfo/src/components/CardBG.js
@@ -20,7 +20,10 @@ const [name, setName] = useState("John Appleseed");
 
 
         <div className="image-stack__item image-stack__item--front">
-            <h3>{cardNum}</h3>
+<svg className="deets icon" viewBox="0 0 100 100" width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/><path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/></svg>
+            <h3 className="deets cardNum">{cardNum}</h3>
+            <h3 className="deets cardName">{name}</h3>
+            <h3 className="deets cardEx">{expiration}</h3>
             </div>
             </div> 
         </>
